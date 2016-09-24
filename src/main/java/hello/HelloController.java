028c459e4195e6ef6cb6d8cb35019b498bcaaf7a
aapeshave <peshave.aj@husky.neu.edu>
2016-09-24 11:49:17 -0400

    Refactoring work

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,18 +1,16 @@
 package hello;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import pojo.Person;
-
 import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pojo.Person;
 
 import javax.ws.rs.GET;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
@@ -22,10 +20,9 @@ public class HelloController {
     @RequestMapping("/person/{id}")
     public String returnPerson(@PathVariable("id") Integer id)
     {
-        System.out.print(id);
         Person p1 = new Person("sumit", "deo", "brighton");
         JSONObject obj = new JSONObject(p1.getPersonAsMap());
         return obj.toJSONString();
     }
-    
+
 }
